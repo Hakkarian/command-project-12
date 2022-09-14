06b3959c272ff657ba5ac6cef997e9bc90ee77b3
Hakkar <zugrutus@gmail.com>
2022-09-14 18:42:18 +0100

    fix5

--- src/js/menualt.js
+++ src/js/menualt.js
@@ -1,6 +1,6 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+  const menuBtnRef = document.querySelector(".menu-button");
+  const mobileMenuRef = document.querySelector(".nav-menu");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
